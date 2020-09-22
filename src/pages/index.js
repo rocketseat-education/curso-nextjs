90a03b0266ae2db38b6e7e652a13e2d842bc0665
Diego Fernandes <diego.schell.f@gmail.com>
2020-09-22 19:52:41 -0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <h1>Hello World</h1>
+    </div>
+  )
+}
